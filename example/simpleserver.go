b74b342bd7f9ced87152b5d23f3d2eb6f7278314
든솔 이 <solsam10@outlook.kr>
2018-11-17 14:26:54 +0900

    [UPDATE] Add ex Server

---
+++ example/simpleserver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Party4Bread/thisis"
+	_ "github.com/go-sql-driver/mysql"
+	"goji.io"
+	. "goji.io/pat"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var INDEX string
+
+func IndexLoad() {
+	tINDEX, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		log.Fatal("Index There is No IndexFile")
+	}
+	INDEX = string(tINDEX)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, INDEX)
+}
+func main() {
+	IndexLoad()
+	DSN := "thisis:thisispasswd@/thisis"
+	//flag.String()
+	//reg := regexp.MustCompile(`^/(?P<lnk>[A-Za-z0-9]+)$`)
+	mux := goji.NewMux()
+	mux.HandleFunc(Get("/"), Index)
+	thisis.SetupServer(mux, DSN)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
